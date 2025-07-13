a57672e3187f2055bf6d8e3a63fa82de0db49e38
hutao33 <hutao33@meituan.com>
2025-07-13 16:18:10 +0800

    初次提交原始仓库代码

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
+        // 查看 IntelliJ IDEA 建议如何修正。
+        System.out.printf("Hello and welcome!");
+
+        System.out.println("v1.0, test add 2 int, add(1, 3) == " + add(1, 3));
+    }
+}
