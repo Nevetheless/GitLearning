0f4e9fa6073a951eb61e25ff4816fd821d976d9b
hutao33 <hutao33@meituan.com>
2025-07-13 20:11:22 +0800

    增加除法功能和测试

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,10 @@ public class Main {
         return a * b;
     }
 
+    public static int div(int a, int b) {
+        return a / b;
+    }
+
     public static void main(String[] args) {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
@@ -15,5 +19,6 @@ public class Main {
 
         System.out.println("v1.0, test add 2 int, add(1, 3) == " + add(1, 3));
         System.out.println("v1.0, test mul 2 int, mul(1, 3) == " + mul(1, 3));
+        System.out.println("v1.0, test div 2 int, div(1, 3) == " + div(3, 1));
     }
 }
