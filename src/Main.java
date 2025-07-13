968b77e163e5f149a0bbf02f6d2d1777be55a3ca
hutao33 <hutao33@meituan.com>
2025-07-13 20:19:40 +0800

    增加func1功能测试

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,10 @@ public class Main {
         return a / b;
     }
 
+    public static int func1() {
+        return -1;
+    }
+
     public static void main(String[] args) {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
