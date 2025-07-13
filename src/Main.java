c786d76ea36b96f097b7f19391d16b439eb51f2a
hutao33 <hutao33@meituan.com>
2025-07-13 16:24:42 +0800

    增加减法功能和测试

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,16 @@ public class Main {
         return a + b;
     }
 
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
         //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
         // 查看 IntelliJ IDEA 建议如何修正。
         System.out.printf("Hello and welcome!");
 
         System.out.println("v1.0, test add 2 int, add(1, 3) == " + add(1, 3));
+        System.out.println("v1.0, test sub 2 int, sub(1, 3) == " + sub(1, 3));
     }
 }
